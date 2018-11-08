757f914809766869ce4f23bf5a5d6f87e252cce1
Nazar Gargol <nazargargol@gmail.com>
2018-11-08 16:50:24 +0100

    Reverted back simple cases to check integration with mingo/knex works

---
+++ test/nql_mingo.test.js
@@ -0,0 +1,26 @@
+require('./utils');
+const nql = require('@nexes/nql-lang');
+const mingo = require('mingo');
+const simpleJSON = require('./fixtures/simple');
+
+/**
+ * The purpose of this file is to prove that NQL
+ * is not just transformed to mongo queries correctly
+ * but that this can be used in real world settings to match JSON
+ */
+
+const makeQuery = (nqlString, options) => {
+    const filter = nql.parse(nqlString, options);
+    return new mingo.Query(filter);
+};
+
+describe('Integration with Mingo', function () {
+    it('should match based on simple id', function () {
+        const query = makeQuery('id:3');
+
+        query.test(simpleJSON.posts[0]).should.eql(false);
+        query.test(simpleJSON.posts[1]).should.eql(false);
+        query.test(simpleJSON.posts[2]).should.eql(true);
+        query.test(simpleJSON.posts[3]).should.eql(false);
+    });
+});
